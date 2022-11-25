0a9e84b858cf04237c406fe39b1d5bd3f3c2034d
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2022-11-25 20:15:48 +0100

    update

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 
 exports.connectDB = async () => {
      try {
-       const conn = await mongoose.connect(process.env.MongoDB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+       const conn = await mongoose.connect(process.env.MONGODB_URI,)
 
        if (process.env.NODE_ENV === 'development'){
          console.log(`MongoDB Connected in development enviroment`)
