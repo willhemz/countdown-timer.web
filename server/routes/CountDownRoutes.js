44b95f21de3336c1f891f4fbdab958a7de40099a
LeRoi Vladimir <banyamkimbi@vrfound.org>
2022-11-25 08:44:51 +0100

    deleting useless lines

--- server/routes/CountDownRoutes.js
+++ server/routes/CountDownRoutes.js
@@ -7,11 +7,5 @@ router.get('/', (req, res) => {
     console.log('up amd running')
     res.status(200)
 })
-// router.get('/:id', getRecipe)
-// router.put('/:id', authenticate, updateRecipe)
-// router.post('/', authenticate, createRecipe)
-// router.delete('/:id', authenticate, deleteRecipe)
-// router.patch('/:id/publish', authenticate, isPublished)
-// router.patch('/:id/vote', authenticate, voteRecipe)
 
 module.exports = router
